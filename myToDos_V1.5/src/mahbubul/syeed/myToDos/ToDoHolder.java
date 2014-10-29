2affb1c1082e4c020cbfd29cbcb11f06f362a287
syeed007 <rajit.cit@gmail.com>
2014-10-29 21:10:17 +0200

    Updated version of myToDo

---
+++ myToDos_V1.5/src/mahbubul/syeed/myToDos/ToDoHolder.java
@@ -0,0 +1,11 @@
+package mahbubul.syeed.myToDos;
+
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class ToDoHolder {
+	TextView holderToDoTitle;
+	CheckBox holderStatusView;
+	TextView holderPriorityView;
+	TextView holderDateView;
+}
